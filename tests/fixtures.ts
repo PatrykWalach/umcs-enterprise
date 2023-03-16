b9a4ef77592114a108a336960bb53496e4a8dbd9
Patryk Wałach <35966385+PatrykWalach@users.noreply.github.com>
2023-03-16 17:52:41 +0100

    chore: run test parallel

---
+++ tests/fixtures.ts
@@ -0,0 +1,25 @@
+import { test as base } from '@playwright/test';
+
+import { preview, type PreviewServer } from 'vite';
+
+export const test = base.extend<
+	Record<never, never>,
+	{
+		app: PreviewServer;
+	}
+>({
+	app: [
+		// eslint-disable-next-line no-empty-pattern
+		async ({}, use) => {
+			const server = await preview({});
+
+			await use(server);
+		},
+		{ scope: 'worker' }
+	],
+
+	async baseURL({ app }, use) {
+		const [address] = app.resolvedUrls.local ?? [];
+		await use(address);
+	}
+});
