30f6bcf8a1cc6420df62ece7def6987d4d3651ca
Patryk Wałach <35966385+PatrykWalach@users.noreply.github.com>
2023-04-09 19:05:33 +0200

    chore: unformat file to test ci

--- src/main/java/com/umcs/enterprise/Book.java
+++ src/main/java/com/umcs/enterprise/Book.java
@@ -34,7 +34,8 @@ public class Book implements Node {
 	@Column(precision = 19, scale = 4)
 	private BigDecimal price;
 
-	private ZonedDateTime createdAt;
+	private ZonedDateTime 
+	createdAt;
 
 	private ZonedDateTime releasedAt;
 
