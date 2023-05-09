d21820f40c279ebf8093bc4b5887d5494da4b0e3
Patryk Wałach <35966385+PatrykWalach@users.noreply.github.com>
2023-05-09 13:21:00 +0200

    chore: fix typo

--- src/routes/Book.svelte
+++ src/routes/Book.svelte
@@ -75,7 +75,7 @@
 				>
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
-						fill_PAD="none"
+						fill="none"
 						viewBox="0 0 24 24"
 						stroke-width="1.5"
 						stroke="currentColor"
