9c81206d3154583f337521cda1219db8be37561b
Patryk Wałach <35966385+PatrykWalach@users.noreply.github.com>
2023-05-12 17:21:22 +0200

    refactor: books order use map struct

--- src/main/java/com/umcs/enterprise/book/BookDataFetcher.java
+++ src/main/java/com/umcs/enterprise/book/BookDataFetcher.java
@@ -4,13 +4,11 @@ import com.netflix.graphql.dgs.*;
 import com.umcs.enterprise.*;
 import com.umcs.enterprise.cover.CoverService;
 
+import com.umcs.enterprise.types.BookOrderBy;
 import graphql.schema.DataFetchingEnvironment;
 import jakarta.persistence.EntityManager;
 import java.io.IOException;
-import java.math.BigDecimal;
-import java.time.ZonedDateTime;
 import java.util.*;
-import java.util.function.BiConsumer;
 
 import lombok.*;
 import org.mapstruct.factory.Mappers;
@@ -24,57 +22,43 @@ public class BookDataFetcher {
 	@NonNull
 	private final BookRepository bookRepository;
 
+
 	@NonNull
 	private final ConnectionService connectionService;
-
-	private <T> Sort getSort(
-		List<T> orders,
-		BiConsumer<HashMap<String, com.umcs.enterprise.types.Order>, T> fn
-	) {
-		return Sort.by(
-			Optional
-				.ofNullable(orders)
-				.orElse(new ArrayList<>())
-				.stream()
-				.map(orderBy -> {
-					HashMap<String, com.umcs.enterprise.types.Order> fields = new HashMap<>();
-					fn.accept(fields, orderBy);
-					return fields;
-				})
-				.flatMap(fields ->
-					fields
-						.entrySet()
-						.stream()
-						.filter(e -> e.getValue() != null)
-						.map(e -> {
-							if (e.getValue() == com.umcs.enterprise.types.Order.ASC) {
-								return Sort.Order.asc(e.getKey());
-							}
-							return Sort.Order.desc(e.getKey());
-						})
-				)
-				.toList()
-		);
-	}
 
 	@DgsQuery
 	public graphql.relay.Connection<Book> books(
 		DataFetchingEnvironment env,
-		@InputArgument List<com.umcs.enterprise.types.BookOrderBy> orderBy
+		@InputArgument List<BookOrderBy> orderBy
 	) {
+
+
+
+		List<Sort.Order> orders = Mappers.getMapper(BookOrderByMapper.class).bookOrderByListToOrders(orderBy);
+
+
+		System.out.println(orders);
+
+		if(orders !=null){
+			return connectionService.getConnection(
+					this.bookRepository.findAll(
+
+							Sort.by(orders
+
+							)
+
+
+			),
+			env
+		);
+		}
+
+
 		return connectionService.getConnection(
 			this.bookRepository.findAll(
-					getSort(
-						orderBy,
-						(fields, order) -> {
-							fields.put(
-								"price",
-								Optional.ofNullable(order.getPrice()).map(com.umcs.enterprise.types.PriceOrderBy::getRaw).orElse(null)
-							);
-							fields.put("popularity", order.getPopularity());
-							fields.put("releasedAt", order.getReleasedAt());
-						}
-					)
+
+
+
 				),
 			env
 		);
@@ -93,6 +77,7 @@ public class BookDataFetcher {
 
 	@NonNull
 	private final CoverService coverService;
+
 
 	@DgsData(parentType = "CreateBookResult")
 	public Book book(DataFetchingEnvironment env) {
---
+++ src/main/java/com/umcs/enterprise/book/BookOrderByMapper.java
@@ -0,0 +1,58 @@
+package com.umcs.enterprise.book;
+
+
+import com.umcs.enterprise.types.BookOrderBy;
+import org.mapstruct.*;
+import org.springframework.data.domain.Sort;
+
+import java.util.*;
+
+@Mapper
+public interface BookOrderByMapper {
+
+
+
+
+
+
+
+
+    Sort.Direction orderToDirection(com.umcs.enterprise.types.Order order);
+
+
+
+     List<Sort.Order> bookOrderByListToOrders(List<BookOrderBy> orderBy);
+
+
+     @Mapping(target="price",source = "price.raw")
+    BookOrderByDTO bookOrderByToBookOrderByDTO(BookOrderBy orderBy);
+
+
+    default Sort.Order bookOrderByToSort(BookOrderByDTO orderBy){
+
+        HashMap<String, Sort.Direction> map = new HashMap<>();
+
+
+        map.put("popularity", orderBy.popularity() );
+        map.put("price", orderBy.price() );
+        map.put("releasedAt", orderBy.releasedAt() );
+
+        System.out.println(map);
+
+
+        return map.entrySet().stream().filter(e->Objects.nonNull(e.getValue())).findAny().map(entry-> new Sort.Order(entry.getValue(), entry.getKey())).orElse(null);
+    };
+
+
+    static record BookOrderByDTO(Sort.Direction popularity, Sort.Direction price, Sort.Direction releasedAt) {
+
+    }
+
+
+
+
+
+
+
+
+}
