c2e37f7371d557c8e8083d1f75e9fdc1ea2970f1
Patryk Wałach <35966385+PatrykWalach@users.noreply.github.com>
2023-06-16 11:05:45 +0200

    fix: use JPQL `<>` operator

--- src/main/java/com/umcs/enterprise/book/BookRepository.java
+++ src/main/java/com/umcs/enterprise/book/BookRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface BookRepository extends JpaRepository<Book, UUID> {
 	@Query(
-		value = "SELECT b FROM BookPurchase t1, BookPurchase t2 JOIN t2.book b WHERE t1.purchase = t2.purchase AND t1.book != t2.book AND t1.book.databaseId = ?1 GROUP BY t1.book, t2.book, b ORDER BY COUNT(t1.purchase) DESC, b.popularity DESC, b.createdAt"
+		value = "SELECT b FROM BookPurchase t1, BookPurchase t2 JOIN t2.book b WHERE t1.purchase = t2.purchase AND t1.book <> t2.book AND t1.book.databaseId = ?1 GROUP BY t1.book, t2.book, b ORDER BY COUNT(t1.purchase) DESC, b.popularity DESC, b.createdAt"
 	)
 	List<Book> findByOrdersBooksDatabaseId(UUID databaseId);
 }
