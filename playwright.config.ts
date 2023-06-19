1ef3e813c7ec73fecbae66d9442adb9002cf9de6
PatrykWalach <PatrykWalach@users.noreply.github.com>
2023-06-19 08:33:04 +0000

    refactor: run format

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,6 @@
 import { devices, PlaywrightTestConfig } from '@playwright/test';
 import * as dotenv from 'dotenv';
-dotenv.config()
+dotenv.config();
 
 const config: PlaywrightTestConfig = {
 	webServer: [
