6475074510fdbc39b2698a2c3829aa4c9e7cc473
PatrykWalach <PatrykWalach@users.noreply.github.com>
2023-06-20 16:28:07 +0000

    refactor: run format

--- src/main/java/com/umcs/enterprise/purchase/payu/PayuService.java
+++ src/main/java/com/umcs/enterprise/purchase/payu/PayuService.java
@@ -69,8 +69,7 @@ public class PayuService {
 			.purchaseToRequest(
 				purchase,
 				posId,
-	
-				 CLIENT_ADDRESS + ":5173/purchase/" + purchase.getId(),
+				CLIENT_ADDRESS + ":5173/purchase/" + purchase.getId(),
 				summableService
 			);
 
